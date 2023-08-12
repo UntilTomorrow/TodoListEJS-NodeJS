7d283125b4e18906b321a551ea6041da2fa494be
UntilTomorrow <herucahyo55@gmail.com>
2023-08-12 19:00:11 +0700

    up

--- server.js
+++ server.js
@@ -11,10 +11,13 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//views
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 // Route
 app.use('/', homeRoutes);
 
-
-app.listen(3000, () => {
+app.listen(5000, () => {
     console.log('runs well@ Port : ' + 5000);
 });
