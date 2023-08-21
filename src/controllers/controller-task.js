d37e8791640ce84a953c83ba6a2fe69ab93dd717
UntilTomorrow <herucahyo55@gmail.com>
2023-08-21 22:59:15 +0700

    update

--- src/controllers/controller-task.js
+++ src/controllers/controller-task.js
@@ -1,4 +1,6 @@
 const config = require('../configs/database');
+const authenticateToken = require('../configs/jwt');
+
 
 let mysql      = require('mysql');
 let pool       = mysql.createPool(config);
@@ -15,28 +17,32 @@ module.exports = {
 
     },
     add(req, res) {
-        let inputDate = req.body.due_date;
-        let parts = inputDate.split("-");
-        let day = parts[0];
-        let month = parts[1];
-        let year = parts[2];
-        let format = `${year}-${month}-${day}`; // -> YYYY-MM-DD
-        let status = req.body.status === 'on' ? true : false; 
-        let data = { 
+        //authenticateToken(req, res, () => {
+          let inputDate = req.body.due_date;
+          let parts = inputDate.split("-");
+          let day = parts[0];
+          let month = parts[1];
+          let year = parts[2];
+          let format = `${year}-${month}-${day}`;
+          let status = req.body.status === 'on' ? true : false; 
+          let data = { 
             task_name: req.body.task_name, 
             description: req.body.description,
             status: status,
             due_date: format 
-        };
-        let sql = "INSERT INTO tasks SET ?";
-        pool.query(sql, data, (err, results) => {
+            
+          };
+          console.log(data)
+          let sql = "INSERT INTO tasks SET ?";
+          pool.query(sql, data, (err, results) => {
             console.log(data);
-          if (err) {
-            res.status(500).json({ success: false, message: "Failed to add task" });
-          } else {
-            res.status(200).json({ success: true, message: "Task added successfully" });
-          }
-        });
+            if (err) {
+              res.status(500).json({ success: false, message: "Failed to add task" });
+            } else {
+              res.status(200).json({ success: true, message: "Task added successfully" });
+            }
+          });
+        //});
       },
     delete(req,res){
         const querySearch = 'SELECT * FROM tasks WHERE id = ?';
@@ -58,5 +64,30 @@ module.exports = {
                 return res.status(404).json({ message: 'Data not found!', success: false });
             }
         });
-    }       
+    },
+    edit(req, res) {
+        console.log("1")
+        let status = req.body.status === 'on' ? true : false;
+      
+        let formattedDueDate = "2023-08-31"; 
+      
+        let data = {
+          task_name: req.body.task_name,
+          description: req.body.description,
+          status: status,
+          due_date: formattedDueDate
+        };
+      
+        let taskId = req.params.id; // Anda perlu mendapatkan ID tugas yang ingin diubah
+      
+        let sql = "UPDATE tasks SET ? WHERE id = ?";
+        pool.query(sql, [data, taskId], (err, results) => {
+          if (err) {
+            return res.status(500).json({ message: 'There is a mistake', error: err });
+          } else {
+            res.status(200).json({ success: true, message: "Task Edited successfully" });
+          }
+        });
+      },
+      
 }
