11109835a6e7aa1ac42136a83114ff224b9d755d
UntilTomorrow <herucahyo55@gmail.com>
2023-09-04 19:52:15 +0700

    update

--- src/routes/router-task.js
+++ src/routes/router-task.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const taskController = require('../controllers').task;
-
+//const jwt = require('../configs/jwt');
 
 router.get('/', taskController.task);
 router.post('/add', taskController.add);
-router.post('/delete/(:id)', taskController.delete);
+router.post('/edit/:id', taskController.edit);
+router.post('/delete/:id', taskController.delete); 
 module.exports = router;
